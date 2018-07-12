819fba8bd2b327ce672902d164da17088204e534
jinzhuming <jinzhuming0308@gmail.com>
2018-07-12 10:12:19 +0800

    搜索暂时返回一个假数据

---
+++ app/controller/search.js
@@ -0,0 +1,12 @@
+const Controller = require('egg').Controller
+const cookDetailCache = require('../data/cook_details_cache.json')
+
+class SearchController extends Controller {
+  async index() {
+    // 获取搜索内容
+    const q = this.ctx.query.q
+    this.ctx.body = [cookDetailCache.id6411]
+  }
+}
+
+module.exports = SearchController
--- app/router.js
+++ app/router.js
@@ -6,6 +6,7 @@
 // key = f762c03a2498c0ca3525701cfef60e23
 module.exports = (app) => {
   const { router, controller } = app
+  router.get('/api/search', controller.search.index)
   router.get('/api/cook/:id', controller.cook.index)
   router.get('/api/cook_detail/:id', controller.cookDetail.index)
   router.get('/api/categores', controller.categores.index)
